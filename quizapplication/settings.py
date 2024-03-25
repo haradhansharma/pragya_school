cef1ee822eaa88124062bf91ea736d714e9f8a1d
haradhansharma <haradhan.sharma@gmail.com>
2024-03-25 18:14:41 +0600

    allowed

--- quizapplication/settings.py
+++ quizapplication/settings.py
@@ -140,18 +140,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Media folder
-MEDIA_ROOT = BASE_DIR / "media"
-
-# media url
-MEDIA_URL = "/media/"
 
 #CREATE STATIC FOLDER
 STATICFILES_DIRS = [
